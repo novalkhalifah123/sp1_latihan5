ae51b6d6eb170ad78f97b70637845e7c72b449b7
novalkhalifah123 <87108201+novalkhalifah123@users.noreply.github.com>
2021-07-22 12:52:34 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+var nama = prompt("Masukan nama anda")
+var nilai = prompt("Masukan nilai")
+if (nilai > 70) {
+    document.write(nama + "Selamat anda lulus dengan nilai" + ' : ' + nilai)
+} else {
+    document.write(nama + "Maaf anda tidak lulus" + ' : ' + nilai)
+}   
